bb956d332cba7680307c5c5a8d3982b16788792e
jmartinn <juamp_m@hotmail.com>
2023-11-01 19:51:25 +0100

    components & svg optimizations

--- app/page.tsx
+++ app/page.tsx
@@ -6,8 +6,9 @@ import {
   TwitterIcon,
 } from "./components/icons";
 import avatar from "app/avatar.jpeg";
+import ViewCounter from "app/blog/view-counter";
 import { Suspense } from "react";
-import { BlogLink } from "./components/blog-link";
+import { getViewsCount } from "lib/metrics";
 
 function Badge(props) {
   return (
@@ -33,6 +34,27 @@ function ArrowIcon() {
         fill="currentColor"
       />
     </svg>
+  );
+}
+
+async function BlogLink({ slug, name }) {
+  const allViews = await getViewsCount();
+
+  return (
+    <a
+      href={`/blog/${slug}`}
+      className="border border-neutral-200 dark:border-neutral-700 bg-neutral-50  dark:bg-neutral-800 rounded flex items-center justify-between px-3 py-4 w-full"
+    >
+      <div className="flex flex-col">
+        <p className="font-semibold text-neutral-900 dark:text-neutral-100">
+          {name}
+        </p>
+        <ViewCounter allViews={allViews} slug={slug} trackView={false} />
+      </div>
+      <div className="text-neutral-700 dark:text-neutral-300">
+        <ArrowIcon />
+      </div>
+    </a>
   );
 }
 
