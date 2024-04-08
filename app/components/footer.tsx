b02d5d6904d2594055b252dea2fc8a1dc3647e3a
jmartinn <juamp_m@hotmail.com>
2024-04-09 01:53:09 +0200

    modified license on footer component

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -39,7 +39,7 @@ export function Footer() {
         </li>
       </ul>
       <p className="mt-8 text-neutral-600 dark:text-neutral-300">
-        © {new Date().getFullYear()} MIT Licensed
+        © {new Date().getFullYear()} Licensed under <a href="https://creativecommons.org/licenses/by-nc-nd/4.0/" target="_blank" rel="noopener noreferrer">CC BY-NC-ND 4.0</a>
       </p>
     </footer>
   );
