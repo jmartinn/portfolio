0fb8f9a508667432138d425a94048a9f8166361c
Juan Pedro Martin <juamp_m@hotmail.com>
2024-12-13 22:57:36 +0100

    chore(deps): update dependencies and publish new article

--- components/layout/sidebar.tsx
+++ components/layout/sidebar.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { LayoutGroup, motion } from "framer-motion";
+import { LayoutGroup, motion } from "motion/react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 
