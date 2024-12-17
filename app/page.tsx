9840c327fb170ddb19ef4213ae99702a734094cf
Juan Pedro Martin <juamp_m@hotmail.com>
2024-12-17 09:23:17 +0100

    fix: adjust badge alignment and update icon dimensions

--- app/page.tsx
+++ app/page.tsx
@@ -87,20 +87,18 @@ export default function Page() {
 
       <p className="prose prose-neutral dark:prose-invert">
         {`I'm a frontend developer currently working at `}
-        <span className="not-prose">
-          <Badge href="https://interamplify.com">
-            <Icons.interamplify />
-            Interamplify
-          </Badge>
-        </span>
+        <Badge href="https://interamplify.com">
+          <Icons.interamplify />
+          Interamplify
+        </Badge>
         {`, leveraging technologies like `}
         <Badge href="https://react.dev">
           <Image
             alt="React logomark"
             src="/react-logo.svg"
-            className="!mr-1"
-            width="14"
-            height="10"
+            className="mr-1 inline-block align-middle"
+            height="16"
+            width="16"
           />
           React
         </Badge>
@@ -109,31 +107,24 @@ export default function Page() {
           <Image
             alt="Next.js logomark"
             src="/next-logo.svg"
-            className="!mr-1"
-            width="14"
-            height="14"
+            className="mr-1 inline-block align-middle"
+            width="16"
+            height="16"
           />
           Next.js
         </Badge>
         {` or `}
         <Badge href="https://typescriptlang.org">
-          <svg
-            width="13"
-            height="13"
-            viewBox="0 0 400 400"
-            xmlns="http://www.w3.org/2000/svg"
-            className="mr-1 inline-flex"
-          >
-            <style>{`.st0{fill:#007acc}.st1{fill:#fff}`}</style>
-            <path className="st0" d="M0 200V0h400v400H0" />
-            <path
-              className="st1"
-              d="M87.7 200.7V217h52v148h36.9V217h52v-16c0-9 0-16.3-.4-16.5 0-.3-31.7-.4-70.2-.4l-70 .3v16.4l-.3-.1zM321.4 184c10.2 2.4 18 7 25 14.3 3.7 4 9.2 11 9.6 12.8 0 .6-17.3 12.3-27.8 18.8-.4.3-2-1.4-3.6-4-5.2-7.4-10.5-10.6-18.8-11.2-12-.8-20 5.5-20 16 0 3.2.6 5 1.8 7.6 2.7 5.5 7.7 8.8 23.2 15.6 28.6 12.3 41 20.4 48.5 32 8.5 13 10.4 33.4 4.7 48.7-6.4 16.7-22 28-44.3 31.7-7 1.2-23 1-30.5-.3-16-3-31.3-11-40.7-21.3-3.7-4-10.8-14.7-10.4-15.4l3.8-2.4 15-8.7 11.3-6.6 2.6 3.5c3.3 5.2 10.7 12.2 15 14.6 13 6.7 30.4 5.8 39-2 3.7-3.4 5.3-7 5.3-12 0-4.6-.7-6.7-3-10.2-3.2-4.4-9.6-8-27.6-16-20.7-8.8-29.5-14.4-37.7-23-4.7-5.2-9-13.3-11-20-1.5-5.8-2-20-.6-25.7 4.3-20 19.4-34 41-38 7-1.4 23.5-.8 30.4 1l-.2.2z"
-            />
-          </svg>
+          <Image
+            alt="TypeScript logomark"
+            src="/typescript-logo.svg"
+            className="mr-1 inline-block align-middle"
+            width="16"
+            height="16"
+          />
           TypeScript
         </Badge>
-        {` to craft seamless, dynamic web experiences. My goal is to bridge the gap between desisgn and functionality.`}
+        {` to craft seamless, dynamic web experiences. My goal is to bridge the gap between design and functionality.`}
       </p>
 
       <div className="prose prose-neutral dark:prose-invert">
