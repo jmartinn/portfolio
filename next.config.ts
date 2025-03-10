11707ec0d25bae52514b6f80534e832cfa096df5
Juan Pedro Martin <juamp_m@hotmail.com>
2025-03-10 21:20:35 +0100

    feat(config): update next image configuration for remote patterns

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,24 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  images: {
+    formats: ['image/avif', 'image/webp'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+        pathname: '**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'i.scdn.co', // For Spotify album art
+        pathname: '**',
+      }
+    ],
+    dangerouslyAllowSVG: true,
+    contentDispositionType: 'attachment',
+    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
+  },
   async headers() {
     return [
       {
